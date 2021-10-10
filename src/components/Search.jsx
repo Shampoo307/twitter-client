f1246f25cc118d03d449d10516e1a5d79592f841
Thomas Crilly <thomas.crilly@blackbook.ai>
2021-10-10 22:08:56 +1000

    make simple search component

---
+++ src/components/Search.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { Form, Button, Row, Col } from 'react-bootstrap';
+
+export default function Search(props) {
+    return (
+        <Form onSubmit={props.handleSearch}>
+            <Form.Group>
+                <Form.Label>{props.label}</Form.Label>
+                <Row>
+                    <Col>
+                        <Form.Control
+                            type="search"
+                            name="searchTerm"
+                            value={props.value}
+                            placeholder={props.placeholder}
+                            onChange={props.onChange}
+                            autoComplete="off"
+                        />
+                    </Col>
+                    <Col xs="auto">
+                        <Button variant="info" type="submit">
+                            {props.submitText}
+                        </Button>
+                    </Col>
+                </Row>
+            </Form.Group>
+        </Form>
+    )
+}
+
