20fbeb9798ba2f72d991c40c5df4beeb82b1c71a
Thomas Crilly <thomas.crilly@blackbook.ai>
2021-10-18 21:38:41 +1000

    call server, show results on page, adjust layout

--- src/pages/Form.jsx
+++ src/pages/Form.jsx
@@ -6,6 +6,7 @@ import Results from "../components/Results";
 export default function Form(props) {
 
     const [ searchValue, setSearchValue ] = useState('');
+    const [ searchResults, setSearchResults ] = useState([]);
 
     const handleSearchChange = (event) => {
         setSearchValue(event.target.value);
@@ -14,26 +15,35 @@ export default function Form(props) {
     const handleSearch = (event) => {
         event.preventDefault();
         // MAKE SEARCH (call server endpoint)
+        fetch(`/search/${searchValue}`)
+            .then(res => res.json())
+            .then(res => {
+                setSearchResults(res.data);
+            });
     };
 
 
 
 
     return (
-        <Container className="form-container">
+        <Container className="form-container" fluid="lg">
             <Row>
-                <Col>
-                    <Search 
-                        handleSearch={handleSearch}
-                        label="Search for a hashtag..."
-                        value={searchValue}
-                        placeholder="Search for a hashtag..."
-                        onChange={handleSearchChange}
-                        submitText="Search"
-                    />
+                <Col className="tweet-search">
+                    <Row>
+                        <Search 
+                            handleSearch={handleSearch}
+                            label="Search for a hashtag..."
+                            value={searchValue}
+                            placeholder="Search for a hashtag..."
+                            onChange={handleSearchChange}
+                            submitText="Search"
+                            />
+                    </Row>
+                    <Row>
+                        { searchResults && <Results results={searchResults} /> }
+                    </Row>
                 </Col>
-                <Col xs={8}>
-                    <Results />
+                <Col xs lg="8">
                 </Col>
             </Row>
         </Container>
