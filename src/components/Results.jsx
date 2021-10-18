e65ae9bdd520fffd1d314e4171ed2efbdd2ea56e
Thomas Crilly <thomas.crilly@blackbook.ai>
2021-10-18 21:38:25 +1000

    make search results component

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,12 +1,38 @@
 import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, ListGroup, Card } from "react-bootstrap";
 
 
 
 export default function Results(props) {
+    const items = props.results.map((item, index) => {
+        return (
+            // TODO
+
+            // Make separate 'SearchResult' component
+            // and feed in data 
+
+
+            // <ListGroup.Item>
+                
+            //     {item.text}
+            // </ListGroup.Item>
+            <Card
+                bg="Light"
+                text="dark"
+                style={{ width: '18rem' }}
+                key={index}
+                className="mb-2"
+            >
+                <Card.Body>
+                    <Card.Header>{item.id}</Card.Header>
+                    <Card.Text>{item.text}</Card.Text>
+                </Card.Body>
+            </Card>
+        )
+    })
     return (
-        <Container className="results-container">
-            
+        <Container className="search-results">
+            { items }
         </Container>
     )
 }
