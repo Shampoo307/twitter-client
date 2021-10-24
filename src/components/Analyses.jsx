b2b402b62ebe83764626c99473580ea426e09fe9
Thomas Crilly <thomas.crilly@blackbook.ai>
2021-10-24 20:31:35 +1000

    placeholder for analysis

---
+++ src/components/Analyses.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Container, Row, Col, ListGroup, Card } from "react-bootstrap";
+
+
+
+export default function Analyses(props) {
+    return (
+        <p>
+            FILL
+        </p>
+    )
+}
