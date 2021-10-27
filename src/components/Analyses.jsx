2bdcd3b3b08c5e2343ca953e1840e15ba5fce426
Hickro <79234709+Hickro@users.noreply.github.com>
2021-10-27 11:23:24 +1000

    Annoyingly Static graph

--- src/components/Analyses.jsx
+++ src/components/Analyses.jsx
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 import { Container, Row, Col, ListGroup, Card } from "react-bootstrap";
+import Chart from 'chart.js/auto';
 
+export default class LineChart extends Component {
 
+	chartRef = React.createRef();
 
-export default function Analyses(props) {
-    return (
-        <p>
-            FILL
-        </p>
-    )
+	componentDidMount() {
+		const ctx = this.chartRef.current.getContext("2d");
+		
+		new Chart(ctx, {
+			type: "line",
+			data: {
+				labels: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+				datasets: [{ 
+					data: [86,114,106,106,107,111,133],
+					label: "Total",
+					borderColor: "#3e95cd",
+					backgroundColor: "#7bb6dd",
+					fill: false,
+				}
+				]
+			},
+		});
+	}
+	render() {
+		return (
+			<div>
+				<canvas
+				id="myChart"
+				ref={this.chartRef}
+				/>
+			</div>
+			)
+	}
 }
